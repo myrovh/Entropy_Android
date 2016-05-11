1ce53035864cf5c95b42bc2093f46473a8c231e0
myrovh <myrovh@yandex.com>
2016-05-11 16:16:41 +1000

    setup default theme

--- app/src/main/java/com/myrovh/entropy_android/MainActivity.java
+++ app/src/main/java/com/myrovh/entropy_android/MainActivity.java
@@ -2,6 +2,9 @@ package com.myrovh.entropy_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.mikepenz.materialize.MaterializeBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Setup Materialize Theme
+        new MaterializeBuilder().withActivity(this).build();
+
+        //Setup Toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
